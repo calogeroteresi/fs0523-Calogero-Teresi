21049a619d7d04deb8eb9289bd135a83a835d0c7
calogeroteresi <caloteresi@gmail.com>
2023-10-04 00:41:49 +0200

    s2g2

--- m1/s2/g2/D2.js
+++ m1/s2/g2/D2.js
@@ -2,24 +2,24 @@
  Scrivi un algoritmo per trovare il più grande tra due numeri interi.
 */
 {
-  function esercizio1(numero1, numero2) {
-    if (numero1 > numero2) {
-      return numero1;
-    } else if (numero2 > numero1) {
-      return numero2;
+  function esercizio1(x, y) {
+    if (x > y) {
+      return x;
+    } else if (y > x) {
+      return y;
     } else {
       return "I due numeri sono uguali";
     }
   }
+  let x = 15;
+  let y = 15;
+  let maggiore = esercizio1(x, y);
+  console.log("Il numero maggiore è:" + maggiore);
+  
   //Si può creare un'altra funzione che riporti e che si ricolleghi alla funzione qui sopra senza dover richiamare l'esercizio1 per modificarlo e il consolelog
-  let numero1 = 15;
-  let numero2 = 15;
-  let piùgrande = esercizio1(numero1, numero2);
-  console.log("Il numero maggiore è:" + piùgrande);
-
-  numero1 = 40;
-  piùgrande = esercizio1(numero1, numero2);
-  console.log("Il numero maggiore è:" + piùgrande);
+  x = 40;
+  maggiore = esercizio1(x,y);
+  console.log("Il numero maggiore è:" + maggiore);
 }
 
 /* ESERCIZIO 2
@@ -137,7 +137,7 @@ if (numero1 >= numero2 && numero1 >= numero3){
   console.log("Ordine decrescente:" + numero1 +","+ numero3 +","+ numero2);
 }
 }
-
+}
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
@@ -153,49 +153,79 @@ console.log(typeof numero1)
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
-
-
-
+{
+function esercizio9(x){
+if(x % 2 === 0) {
+  return "Pari";
+} else{
+  return "Dispari";
+}
+} 
+ let x = 5
+ let confronto = esercizio9(x);
+ console.log("Il numero " + x + " è " + confronto +" .")
+}
 
 
 
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
+*/
+{  let val = 12
+    if (val < 10 && val > 5) {
       console.log("Meno di 10");
     } else if (val < 5) {
       console.log("Meno di 5");
     } else {
       console.log("Uguale a 10 o maggiore");
     }
-*/
+  }
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
 
-/*const me = {
+const me = {
   name: 'John',
   lastName: 'Doe',
   skills: ['javascript', 'html', 'css'],
 }
-/*
-
+me.city = 'Toronto';
+console.log(me.city)
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
 
+delete me.lastName;
+
+
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
+
+me.skills = me.skills.filter(skill => skill !== "css");
+console.log(me);
+
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
+let esercizio14 = [];
+for (let numero = 1; numero <= 10; numero++){
+  esercizio14.push(numero);
+}
+console.log(esercizio14);
+
+
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
+
+if (esercizio14.length > 0){
+  esercizio14[esercizio14.length- 1] = 100;
+}
+
+console.log(esercizio14);
