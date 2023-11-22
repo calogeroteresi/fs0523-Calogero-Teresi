085ed17681c47f1e2e622137da9b9f88c8de2b3d
calogeroteresi <caloteresi@gmail.com>
2023-11-22 16:44:47 +0100

    lavoratore

--- m3/S1/G2/script.js
+++ m3/S1/G2/script.js
@@ -61,64 +61,57 @@ class MotherAccount extends Account {
         this.deposit(interest);
     }
 }
+class Cerca {
+    static handleSubmit(event) {
+        event.preventDefault();
+        const accountIdInput = document.getElementById('accountId');
+        const accountId = accountIdInput.value;
+        Cerca.searchAccount(accountId);
+    }
+    static searchAccount(accountId) {
+        const accountIdInput = document.getElementById('accountId');
+        const currentBalanceSpan = document.getElementById('currentBalance');
+        accountId = accountIdInput.value;
+        let account;
+        if (accountId === '451Q') {
+            account = sonAccount;
+        }
+        else if (accountId === '541Q') {
+            account = motherAccount;
+        }
+        if (account) {
+            currentBalanceSpan.textContent = account.getBalance().toString();
+            const accountDetailsDiv = document.getElementById('accountDetails');
+            accountDetailsDiv.style.display = 'block';
+        }
+        else {
+            alert('Account non trovato.');
+        }
+    }
+}
+class Gestione {
+    static deposit() {
+        const depositAmountInput = document.getElementById('depositAmount');
+        const amount = parseFloat(depositAmountInput.value);
+        sonAccount.oneDeposit(amount);
+        depositAmountInput.value = '';
+        Gestione.updateBalance();
+    }
+    static withdraw() {
+        const withdrawAmountInput = document.getElementById('withdrawAmount');
+        const amount = parseFloat(withdrawAmountInput.value);
+        sonAccount.oneWithdraw(amount);
+        withdrawAmountInput.value = '';
+        Gestione.updateBalance();
+    }
+    static updateBalance() {
+        const currentBalanceSpan = document.getElementById('currentBalance');
+        const currentBalance = sonAccount.getBalance();
+        currentBalanceSpan.textContent = currentBalance.toString();
+    }
+}
 // Esempio di utilizzo:
 const sonAccount = new SonAccount(0, 'Mario', 'Rossi', 'TTVVCYCBBC', '451Q');
 const motherAccount = new MotherAccount(0, 'Giulia', 'Tramontana', 'VAGAYGYA788AH', '541U');
-sonAccount.oneDeposit(100);
-sonAccount.oneWithdraw(20);
-sonAccount.twoDeposit(100);
-sonAccount.twoWithdraw(20);
-motherAccount.oneDeposit(100);
-motherAccount.oneWithdraw(20);
-motherAccount.twoDeposit(500);
-motherAccount.twoWithdraw(50);
-motherAccount.addInterest();
-console.log('Saldo conto figlio:', sonAccount.getBalance());
-console.log('Saldo conto madre:', motherAccount.getBalance());
-function searchAccount(accountId) {
-    const accountIdInput = document.getElementById('accountId');
-    const currentBalanceSpan = document.getElementById('currentBalance');
-    accountId = accountIdInput.value;
-    let account;
-    if (accountId === '451Q') {
-        account = sonAccount;
-    }
-    else if (accountId === '541Q') {
-        account = motherAccount;
-    }
-    if (account) {
-        currentBalanceSpan.textContent = account.getBalance().toString();
-        const accountDetailsDiv = document.getElementById('accountDetails');
-        accountDetailsDiv.style.display = 'block';
-    }
-    else {
-        alert('Account non trovato.');
-    }
-}
-function deposit() {
-    const depositAmountInput = document.getElementById('depositAmount');
-    const amount = parseFloat(depositAmountInput.value);
-    sonAccount.oneDeposit(amount);
-    depositAmountInput.value = '';
-    updateBalance();
-}
-function withdraw() {
-    const withdrawAmountInput = document.getElementById('withdrawAmount');
-    const amount = parseFloat(withdrawAmountInput.value);
-    sonAccount.oneWithdraw(amount);
-    withdrawAmountInput.value = '';
-    updateBalance();
-}
-function handleSubmit(event) {
-    event.preventDefault();
-    const accountIdInput = document.getElementById('accountId');
-    const accountId = accountIdInput.value;
-    searchAccount(accountId);
-}
-function updateBalance() {
-    const currentBalanceSpan = document.getElementById('currentBalance');
-    const currentBalance = sonAccount.getBalance();
-    currentBalanceSpan.textContent = currentBalance.toString();
-}
 const accountForm = document.getElementById('accountForm');
-accountForm.addEventListener('submit', handleSubmit);
+accountForm.addEventListener('submit', Cerca.handleSubmit);
--- m3/S1/G2/script.ts
+++ m3/S1/G2/script.ts
@@ -97,90 +97,82 @@ class MotherAccount extends Account {
     }
 }
 
+class Cerca{
+    static handleSubmit(event: Event): void {
+        event.preventDefault();
+     
+        const accountIdInput = document.getElementById('accountId') as HTMLInputElement;
+        const accountId = accountIdInput.value;
+     
+        Cerca.searchAccount(accountId);
+     }
+     static searchAccount(accountId: string): void {
+        const accountIdInput = document.getElementById('accountId') as HTMLInputElement;
+        const currentBalanceSpan = document.getElementById('currentBalance') as HTMLSpanElement;
+     
+        accountId = accountIdInput.value;
+        let account: Account | undefined;
+     
+        
+        if (accountId === '451Q') {
+            account = sonAccount;
+        } else if (accountId === '541Q') {
+            account = motherAccount;
+        }
+     
+        if (account) {
+            
+            currentBalanceSpan.textContent = account.getBalance().toString();
+            const accountDetailsDiv = document.getElementById('accountDetails') as HTMLDivElement;
+            accountDetailsDiv.style.display = 'block';
+        } else {
+            
+            alert('Account non trovato.');
+        }
+     }
+     
+
+}
+
+class Gestione {
+    static deposit(): void {
+        const depositAmountInput = document.getElementById('depositAmount') as HTMLInputElement;
+        const amount = parseFloat(depositAmountInput.value);
+     
+        sonAccount.oneDeposit(amount);
+     
+        depositAmountInput.value = '';
+     
+        Gestione.updateBalance();
+     }
+
+    static withdraw(): void {
+        const withdrawAmountInput = document.getElementById('withdrawAmount') as HTMLInputElement;
+        const amount = parseFloat(withdrawAmountInput.value);
+     
+        sonAccount.oneWithdraw(amount); 
+     
+        withdrawAmountInput.value = '';
+     
+        Gestione.updateBalance();
+     }
+     
+     static updateBalance(): void {
+        const currentBalanceSpan = document.getElementById('currentBalance') as HTMLSpanElement;
+     
+        const currentBalance = sonAccount.getBalance(); 
+     
+        currentBalanceSpan.textContent = currentBalance.toString();
+     }
+
+
+}
+
+
 // Esempio di utilizzo:
 const sonAccount = new SonAccount(0, 'Mario', 'Rossi', 'TTVVCYCBBC', '451Q');
 const motherAccount = new MotherAccount(0, 'Giulia', 'Tramontana', 'VAGAYGYA788AH', '541U');
 
-sonAccount.oneDeposit(100);
-sonAccount.oneWithdraw(20);
-sonAccount.twoDeposit(100);
-sonAccount.twoWithdraw(20);
-motherAccount.oneDeposit(100);
-motherAccount.oneWithdraw(20);
-motherAccount.twoDeposit(500);
-motherAccount.twoWithdraw(50);
-motherAccount.addInterest();
-
-console.log('Saldo conto figlio:', sonAccount.getBalance());
-console.log('Saldo conto madre:', motherAccount.getBalance());
-
-
-function searchAccount(accountId: string): void {
-   const accountIdInput = document.getElementById('accountId') as HTMLInputElement;
-   const currentBalanceSpan = document.getElementById('currentBalance') as HTMLSpanElement;
-
-   accountId = accountIdInput.value;
-   let account: Account | undefined;
-
-   
-   if (accountId === '451Q') {
-       account = sonAccount;
-   } else if (accountId === '541Q') {
-       account = motherAccount;
-   }
-
-   if (account) {
-       
-       currentBalanceSpan.textContent = account.getBalance().toString();
-       const accountDetailsDiv = document.getElementById('accountDetails') as HTMLDivElement;
-       accountDetailsDiv.style.display = 'block';
-   } else {
-       
-       alert('Account non trovato.');
-   }
-}
-
-function deposit(): void {
-   const depositAmountInput = document.getElementById('depositAmount') as HTMLInputElement;
-   const amount = parseFloat(depositAmountInput.value);
-
-   sonAccount.oneDeposit(amount);
-
-   depositAmountInput.value = '';
-
-   updateBalance();
-}
-
-
-function withdraw(): void {
-   const withdrawAmountInput = document.getElementById('withdrawAmount') as HTMLInputElement;
-   const amount = parseFloat(withdrawAmountInput.value);
-
-   sonAccount.oneWithdraw(amount); 
-
-   withdrawAmountInput.value = '';
-
-   updateBalance();
-}
-
-
-function handleSubmit(event: Event): void {
-   event.preventDefault();
-
-   const accountIdInput = document.getElementById('accountId') as HTMLInputElement;
-   const accountId = accountIdInput.value;
-
-   searchAccount(accountId);
-}
-
-
-function updateBalance(): void {
-   const currentBalanceSpan = document.getElementById('currentBalance') as HTMLSpanElement;
-
-   const currentBalance = sonAccount.getBalance(); 
-
-   currentBalanceSpan.textContent = currentBalance.toString();
-}
 
 const accountForm = document.getElementById('accountForm') as HTMLFormElement;
-accountForm.addEventListener('submit', handleSubmit);
+accountForm.addEventListener('submit', Cerca.handleSubmit);
